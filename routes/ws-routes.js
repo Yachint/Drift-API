adf6c879f8bf7ec403b3fd91413d575d7b7ee46b
Yachint <yachint.yadav@gmail.com>
2021-07-27 20:50:59 +0530

    Web socket improvements

--- routes/ws-routes.js
+++ routes/ws-routes.js
@@ -8,8 +8,12 @@ router.ws("/checkUpdate", function (ws, req) {
     console.log(
       `User: ${req.socket.remoteAddress} requested hash for ${parsedMessage.woeid}`
     );
-    let hash = trends.provideCurrentHash(parsedMessage.woeid);
-    const reply = { hash };
+    let hashRegional = trends.provideCurrentHash(parsedMessage.woeid);
+    let hashGlobal = trends.provideCurrentHash(1);
+    const reply = {
+      regional: hashRegional,
+      global: hashGlobal,
+    };
     ws.send(JSON.stringify(reply));
   });
 
